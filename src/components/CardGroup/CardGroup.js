4df1f098bc05b4b6ba5605e653db007b94cb0479
Nicolas Maximiliano Martin <n.martin@corp.smtsoftware.com>
2021-11-04 16:35:51 -0300

    feature/an/cardGroup

--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -3,11 +3,11 @@ import './CardGroup.css'
 import Card from '../Cards/Card';
 import PropTypes from 'prop-types';
 
-const CardGroup = ({cards = [2]}) => {
+const CardGroup = ({cards = [{title: "work", hours: "2hs", lasthours: "5hs"},{title: "play", hours: "2hs", lasthours: "5hs"},{title: "study", hours: "2hs", lasthours: "5hs"}]}) => {
     return(
         <div className="card-group__container">
             {cards.map(item => (
-                <Card title={item.title} hours={item.hours} lasthours={item.lasthours} />
+                <Card title={item.title} hours={item.hours} lasthours={item.lasthours}/>
             ))}
         </div>
     )
--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './Card.css';
 
+
 const Card = ({title, hours, lasthours}) => {
 
     return (
-        <div className="card-container">
+        <div className={`card card-container ${activity}`}>
             <div className="card-small">
                 <div className="card-main">
                     <h3 className="card-title">{title}</h3>
-                    <img src="" alt="" className="card-image" />
+                    <img src="icon-ellipsis.a57a4ac8.svg" alt="ellipsis" className="card-image" />
                 </div>
                 <div className="card-details">
                     <p className="card-currentTime">{hours}</p>
@@ -23,7 +24,7 @@ const Card = ({title, hours, lasthours}) => {
 Card.propTypes = {
     title: PropTypes.string,
     hours: PropTypes.string,
-    lasthours: PropTypes.string,
+    lasthours: PropTypes.string
 }
 
 export default Card
